3913fc14b9ae3e09720b80e40e98757ddefa277c
Michael Griffin <mdgriffin064@gmail.com>
2019-02-25 11:43:23 +0000

    Initial Commit

---
+++ src/main/java/com/mdgriffin/distributedcomputingproject/Application.java
@@ -0,0 +1,12 @@
+package com.mdgriffin.distributedcomputingproject;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+
+    public int stub () {
+        return 1;
+    }
+}
---
+++ src/test/java/com/mdgriffin/distributedcomputingproject/ApplicationTest.java
@@ -0,0 +1,14 @@
+package com.mdgriffin.distributedcomputingproject;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ApplicationTest {
+
+    @Test
+    void stub() {
+        Application app = new Application();
+        assertEquals(1, app.stub());
+    }
+}
