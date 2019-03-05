3ccecf0db15043ac64949cf187cd3d66a5a50598
Michael Griffin <mdgriffin064@gmail.com>
2019-03-05 15:38:49 +0000

    Removed codes from Request and Response

--- src/main/java/com/mdgriffin/distributedcomputingproject/common/RequestCodes.java
+++ src/main/java/com/mdgriffin/distributedcomputingproject/common/RequestCodes.java
@@ -1,19 +1,9 @@
 package com.mdgriffin.distributedcomputingproject.common;
 
 public enum RequestCodes {
-    LOGIN(100),
-    LOGOFF(101),
-    UPLOAD(102),
-    DOWNLOAD(103),
-    LIST(104);
-
-    private int code;
-
-    RequestCodes(int code) {
-        this.code = code;
-    }
-
-    public int getCode () {
-        return this.code;
-    }
+    LOGIN,
+    LOGOFF,
+    UPLOAD,
+    DOWNLOAD,
+    LIST;
 }
--- src/main/java/com/mdgriffin/distributedcomputingproject/common/ResponseCodes.java
+++ src/main/java/com/mdgriffin/distributedcomputingproject/common/ResponseCodes.java
@@ -1,19 +1,9 @@
 package com.mdgriffin.distributedcomputingproject.common;
 
 public enum ResponseCodes {
-    SUCCESS(101),
-    ERROR(102),
-    DENIED(103),
-    NOT_FOUND(104);
-
-    private int code;
-
-    ResponseCodes(int code) {
-        this.code = code;
-    }
-
-    public int getCode() {
-        return this.code;
-    }
+    SUCCESS,
+    ERROR,
+    DENIED,
+    NOT_FOUND;
 
 }
