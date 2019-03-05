39d0c54bd0b57063d6a1c56afc5efabd5ff08c45
Michael Griffin <mdgriffin064@gmail.com>
2019-03-05 10:16:11 +0000

    Added pacakges for client and server

---
+++ src/main/java/com/mdgriffin/distributedcomputingproject/client/Application.java
@@ -0,0 +1,7 @@
+package com.mdgriffin.distributedcomputingproject.client;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Client Main");
+    }
+}
---
+++ src/main/java/com/mdgriffin/distributedcomputingproject/server/Application.java
@@ -0,0 +1,9 @@
+package com.mdgriffin.distributedcomputingproject.server;
+
+public class Application {
+
+
+    public static void main(String[] args) {
+        System.out.println("Server Main");
+    }
+}
