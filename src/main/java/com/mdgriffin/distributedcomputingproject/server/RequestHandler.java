d839169efc12df624fb14cf675f7f817d6f417fa
Michael Griffin <mdgriffin064@gmail.com>
2019-03-17 00:06:12 +0000

    Removed unused imports

--- src/main/java/com/mdgriffin/distributedcomputingproject/server/RequestHandler.java
+++ src/main/java/com/mdgriffin/distributedcomputingproject/server/RequestHandler.java
@@ -1,9 +1,6 @@
 package com.mdgriffin.distributedcomputingproject.server;
 
-import com.mdgriffin.distributedcomputingproject.common.DatagramMessage;
 import com.mdgriffin.distributedcomputingproject.common.Message;
-
-import java.io.IOException;
 
 public interface RequestHandler {
 
