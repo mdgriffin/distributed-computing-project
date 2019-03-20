68aff46665942e52f30a87b20b4befe7658baf1d
Michael Griffin <mdgriffin064@gmail.com>
2019-03-20 12:23:33 +0000

    Running login asynchronously in a runnable

--- src/main/java/com/mdgriffin/distributedcomputingproject/gui/LoginController.java
+++ src/main/java/com/mdgriffin/distributedcomputingproject/gui/LoginController.java
@@ -3,6 +3,7 @@ package com.mdgriffin.distributedcomputingproject.gui;
 import com.mdgriffin.distributedcomputingproject.client.ClientHandler;
 import com.mdgriffin.distributedcomputingproject.common.Message;
 import com.mdgriffin.distributedcomputingproject.common.Response;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
@@ -33,26 +34,31 @@ public class LoginController {
         if (username.length() == 0 || password.length() == 0) {
             lblLoginError.setText("Please provide a value for username and password");
         } else {
-            try {
-                Message loginMessage = clientHandler.login(username, password);
+            Platform.runLater(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        Message loginMessage = clientHandler.login(username, password);
 
-                if (loginMessage.getResponse().equals(Response.SUCCESS)) {
-                    clientHandler.setSessionId(loginMessage.getHeaderValue("session_id"));
+                        if (loginMessage.getResponse().equals(Response.SUCCESS)) {
+                            clientHandler.setSessionId(loginMessage.getHeaderValue("session_id"));
 
-                    Alert alert = new Alert(Alert.AlertType.INFORMATION, "Logged In Successfully");
-                    alert.showAndWait();
+                            Alert alert = new Alert(Alert.AlertType.INFORMATION, "Logged In Successfully");
+                            alert.showAndWait();
 
-                    if (context != null) {
-                        context.changeToFileManager();
+                            if (context != null) {
+                                context.changeToFileManager();
+                            }
+                        } else if (loginMessage.getResponse().equals(Response.DENIED)) {
+                            lblLoginError.setText("Incorrect username or password");
+                        } else {
+                            lblLoginError.setText("Error Logging In");
+                        }
+                    } catch (IOException exc) {
+                        lblLoginError.setText("Error Logging In");
                     }
-                } else if (loginMessage.getResponse().equals(Response.DENIED)) {
-                    lblLoginError.setText("Incorrect username or password");
-                } else {
-                    lblLoginError.setText("Error Logging In");
                 }
-            } catch (IOException exc) {
-                lblLoginError.setText("Error Logging In");
-            }
+            });
         }
 
     }
